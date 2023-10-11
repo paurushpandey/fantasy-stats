64ad4e75d9ec12c0aa916aa49fa73632bf021cb9
Chubtato <pandeypaurush@gmail.com>
2023-10-11 00:58:38 -0700

    Gets sleeper data and updates allTimeOverUnder correctly. Need to make it so the graph displays correctly.

--- components/LineGraph.js
+++ components/LineGraph.js
@@ -22,6 +22,8 @@ const LineGraph = ({ data }) => {
     data: data[categoryName],
     borderColor: getRandomColor(),
     fill: false,
+    tension: 0.2,
+    borderJoinStyle: 'round',
   }));
 
   const chartData = {
@@ -46,6 +48,27 @@ const LineGraph = ({ data }) => {
         },
       },
     },
+    onHover: (e, activeEls, chart) => {
+        if (activeEls.length === 0) {
+          chart.data.datasets.forEach((dataset) => {
+            // dataset.backgroundColor = dataset.backgroundColor.length === 9 ? dataset.backgroundColor.slice(0, -2) : dataset.backgroundColor;
+            dataset.borderColor = dataset.borderColor.length === 9 ? dataset.borderColor.slice(0, -2) : dataset.borderColor;
+          });
+          chart.update();
+          return;
+        }
+  
+        const hoveredEl = chart.getElementsAtEventForMode(e, 'point', {
+          intersect: true
+        }, true)[0]
+  
+        chart.data.datasets.forEach((dataset, i) => {
+        //   dataset.backgroundColor = (hoveredEl.datasetIndex === i || dataset.backgroundColor.length === 9) ? dataset.backgroundColor : dataset.backgroundColor + '4D';
+          dataset.borderColor = (hoveredEl.datasetIndex === i || dataset.borderColor.length === 9) ? dataset.borderColor : dataset.borderColor + '4D';
+        });
+  
+        chart.update();
+      },
 }
 
   return <Line data={chartData} options={options} />;
--- pages/winloss.js
+++ pages/winloss.js
@@ -1,6 +1,7 @@
 // pages/fantasyData.tsx
 import React, { useEffect, useState } from "react";
 import LineGraph from "../components/LineGraph";
+import axios from "axios";
 
 const teamIDs = {
   2019: {
@@ -47,8 +48,26 @@ const teamIDs = {
     9: "Ved",
     10: "Sujay",
   },
+  sleeper: {
+    0: "Chubs",
+    1: "Waff",
+    2: "Sujay",
+    3: "Avi",
+    4: "Eeswar",
+    5: "Romo",
+    6: "Rohan",
+    7: "Keg",
+    8: "Santosh",
+    9: "Ved",
+    10: "Thomas",
+    11: "Arhan",
+  },
 };
 
+const sleeperLeagueIds = {
+  2022: "860413600286744576",
+  2023: "988741650312704000",
+};
 const allTimeOverUnder = {
   Chubs: [],
   Waff: [],
@@ -61,17 +80,22 @@ const allTimeOverUnder = {
   Ved: [],
   Sujay: [],
   Sumanth: [],
+  Thomas: [],
+  Eeswar: [],
 };
 
 const updateData = (allTimeOverUnder, team, won, setData) => {
   // console.log("hi")
+  // console.log(allTimeOverUnder)
+  // console.log(team);
+  // console.log(allTimeOverUnder[team]);
   let len = allTimeOverUnder[team].length;
-  if (len===0){
+  if (len === 0) {
     allTimeOverUnder[team].push(won);
-  }
-  else allTimeOverUnder[team].push(allTimeOverUnder[team][len - 1] + won);
+  } else allTimeOverUnder[team].push(allTimeOverUnder[team][len - 1] + won);
 };
-const addSeasonalData= async (startWeek,endWeek,year,myClient,setData)=>{
+
+const addSeasonalData = async (startWeek, endWeek, year, myClient, setData) => {
   for (let week = startWeek; week <= endWeek; week++) {
     const weeklyScores = {
       Chubs: 0,
@@ -85,6 +109,8 @@ const addSeasonalData= async (startWeek,endWeek,year,myClient,setData)=>{
       Ved: 0,
       Sujay: 0,
       Sumanth: 0,
+      Thomas: 0,
+      Eeswar: 0,
     };
     try {
       const teams = await myClient.getHistoricalScoreboardForWeek({
@@ -114,27 +140,118 @@ const addSeasonalData= async (startWeek,endWeek,year,myClient,setData)=>{
       updateData(allTimeOverUnder, team, weeklyScores[team], setData);
     }
   }
-}
+};
+
+const sleeperAddSeasonalData = async (endWeek, year, setData) => {
+  return new Promise((resolve, reject) => {
+    let completedWeeks = 0;
+    for (let week = 1; week <= endWeek; week++) {
+      // weeklyMatchups = {matchupId:{homeTeamId, awayTeamId, homeTeamScore, awayTeamScore}};
+      const weeklyMatchups = [{}, {}, {}, {}, {}, {}];
+      const weeklyScores = {
+        Chubs: 0,
+        Waff: 0,
+        Rohan: 0,
+        Santosh: 0,
+        Romo: 0,
+        Arhan: 0,
+        Keg: 0,
+        Avi: 0,
+        Ved: 0,
+        Sujay: 0,
+        Sumanth: 0,
+        Thomas: 0,
+        Eeswar: 0,
+      };
+      const findMedian = [];
+      try {
+        completedWeeks++;
+        if (completedWeeks === endWeek) {
+          resolve(); // Resolve the promise when all weeks are processed
+        }
+        axios
+          .get(
+            `https://api.sleeper.app/v1/league/${sleeperLeagueIds[year]}/matchups/${week}`
+          )
+          .then((response) => {
+            // Handle the API response data here
+            response.data.forEach((matchup) => {
+              const matchup_id = matchup.matchup_id - 1;
+              if (weeklyMatchups[matchup_id].homeTeamId !== undefined) {
+                weeklyMatchups[matchup_id].awayTeamId = matchup.roster_id - 1;
+                weeklyMatchups[matchup_id].awayTeamScore = matchup.points;
+              } else {
+                weeklyMatchups[matchup_id] = {
+                  homeTeamId: matchup.roster_id - 1,
+                  homeTeamScore: matchup.points,
+                };
+              }
+              findMedian.push(matchup.points);
+            });
+
+            findMedian.sort((a, b) => a - b);
+            const medianScore = (findMedian[5] + findMedian[6]) / 2;
+
+            console.log(year, week);
+            // console.log(weeklyMatchups);
+
+            for (const matchupId in weeklyMatchups) {
+              const matchup = weeklyMatchups[matchupId];
+              const homeTeamScore = matchup.homeTeamScore;
+              const awayTeamScore = matchup.awayTeamScore;
+              const homeTeam = teamIDs.sleeper[matchup.homeTeamId];
+              const awayTeam = teamIDs.sleeper[matchup.awayTeamId];
+
+              if (homeTeamScore > awayTeamScore) {
+                weeklyScores[homeTeam] = 1;
+                weeklyScores[awayTeam] = -1;
+              } else {
+                weeklyScores[homeTeam] = -1;
+                weeklyScores[awayTeam] = 1;
+              }
+              if (homeTeamScore > medianScore) weeklyScores[homeTeam]++;
+              else weeklyScores[homeTeam]--;
+              if (awayTeamScore > medianScore) weeklyScores[awayTeam]++;
+              else weeklyScores[awayTeam]--;
+            }
+            for (const team in weeklyScores) {
+              updateData(allTimeOverUnder, team, weeklyScores[team], setData);
+            }
+          })
+          .catch((error) => {
+            // Handle errors here
+            console.error("Error:", error);
+          });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        reject(error);
+      }
+    }
+  });
+};
+
 const WinLoss = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    // Create a Client instance and set the cookies
     const initClient = async () => {
       const { Client } = await import("espn-fantasy-football-api");
       const myClient = new Client({ leagueId: 91791069 });
-      await addSeasonalData(3,12,2019,myClient,setData);
-      await addSeasonalData(1,12,2020,myClient,setData);
-      await addSeasonalData(1,14,2021,myClient,setData);
-      setData(allTimeOverUnder);
-      // console.log(allTimeOverUnder);
+      await addSeasonalData(3, 12, 2019, myClient, setData); // streamline later so it doesnt take so long, these can be synchronous
+      await addSeasonalData(1, 12, 2020, myClient, setData);
+      await addSeasonalData(1, 14, 2021, myClient, setData);
+      await sleeperAddSeasonalData(14, 2022, setData);
+      await sleeperAddSeasonalData(4, 2023, setData);
     };
-    initClient();
+    initClient().then(() => {
+      setData(allTimeOverUnder);
+      console.log(allTimeOverUnder);
+    });
   }, []);
 
   return (
     <div>
-      <h1>Fantasy Football Data</h1>
+      <h1>Regular Season Win Loss Differential</h1>
       {/* {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>} */}
       {data ? <LineGraph data={allTimeOverUnder} /> : <p>Loading...</p>}
     </div>
